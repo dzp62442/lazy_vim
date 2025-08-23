f93702d26dafe495163940b2f850588300d1bb05
dzp62442 <dzp62442@163.com>
2025-08-23 13:44:40 +0800

    使用 Alt + / 打开关闭终端

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 使用 Alt + / 打开关闭终端
+vim.keymap.set("n", "<A-/>", function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("t", "<A-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
