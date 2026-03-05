60320f9fe568ec5858a0b02748c99a1fb5652719
dzp62442 <dzp62442@163.com>
2026-03-05 16:17:14 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Keep English spell-checking, but don't mark CJK chars as misspelled.
+vim.opt.spelllang = { "en", "cjk" }
+
 -- WSL clipboard integration (copy + paste with Windows clipboard)
 if vim.fn.has("wsl") == 1 then
   vim.g.clipboard = {
